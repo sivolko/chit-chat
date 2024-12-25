bbc251e091b30846b157be2d9070901c85a073d9
sivolko <sivolko@kali>
2024-12-25 14:37:09 +0530

    first

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Enable CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.get('/api/get-github-token', (req, res) => {
+    const token = process.env.GITHUB_API_TOKEN;
+    if (token) {
+        res.json({ token });
+    } else {
+        res.status(500).json({ error: 'GitHub API token not found' });
+    }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
